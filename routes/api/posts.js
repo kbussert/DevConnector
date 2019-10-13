4af9dbfe85f21f98b43d039b358c69d09b8b14de
Kyle Bussert <kbussert@white.syrres.com>
2019-10-13 12:52:05 -0400

    Adding ability for users to like or remove a like from a post.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -125,4 +125,58 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+// @route  POST api/posts/like/:id
+// @desc   Like a post
+// @access Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if the post has already been liked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({
+                msg: 'Post already liked'
+            });
+        }
+
+        post.likes.unshift({
+            user: req.user.id
+        });
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route  POST api/posts/unlike/:id
+// @desc   Remove like from a post
+// @access Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if the post has already been liked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({
+                msg: 'Post not yet liked'
+            });
+        }
+
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
